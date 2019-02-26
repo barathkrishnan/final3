1f40a6b06257d541badf8551ea3cb9beb1e08d3d
kannimuthud <46344606+kannimuthud@users.noreply.github.com>
2019-02-26 11:19:45 +0530

    Update App.js

--- App.js
+++ App.js
@@ -118,7 +118,7 @@ state = {
       messages: [
         {
           _id: 1,
-          text: 'Welcome',
+          text: 'Hello This is Watson How can I help you?',
           
           createdAt: new Date(),
           user: {
